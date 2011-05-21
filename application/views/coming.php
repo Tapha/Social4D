0b903ccb477d0f10a2354d03fc665595bbf2b868
Tapha Ngum <tapha@live.co.uk>
2011-05-21 14:29:15 +0300

    added all codeigniter stuff

---
+++ application/views/coming.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Social4D - "4-Dimensional Retargeted Social Brand Advertising"</title>
+	<link rel="icon" type="image/png" href="<?php echo $base_url."img/favicon.ico";?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo $base_url.'css/style.css';?>" />
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.0/jquery.min.js" type="text/javascript"></script>
+	<script src="<?php echo $base_url.'js/script.js';?>" type="text/javascript"></script>
+</head>
+<body>
+<div id='wrapper'>
+    <div id='header'>
+        <div id='logo'>
+            <img src="<?php echo $base_url.'img/logo.png';?>">
+        </div>
+        <div id='sub_headline'>
+            <h1><i>"4-Dimensional Retargeted Social Brand Advertising"</i></h1>
+        </div>
+        <div id='sub_headline_2'>
+            <h1>-Bringing it. Summer 2011-</h1>
+        </div>
+    </div>    
+</div>
+</body>
+</html>
